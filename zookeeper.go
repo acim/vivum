dfe7a39303aefb46dea2eed31310ebcfe0f0f731
Boban Acimovic <boban.acimovic@gmail.com>
2018-08-30 20:32:04 +0200

    implement ChildrenC

--- zookeeper.go
+++ zookeeper.go
@@ -1,4 +1,4 @@
-package zkv
+package zkc
 
 import (
 	"time"
@@ -6,12 +6,12 @@ import (
 	"github.com/samuel/go-zookeeper/zk"
 )
 
-// Conn extends go-zookeeper functionality.
+// Conn extends go-zookeeper's functionality.
 type Conn struct {
 	*zk.Conn
 }
 
-// New creates new *Conn instance.
+// New creates new *zkc.Conn instance.
 func New(servers []string, sessionTimeout time.Duration) (*Conn, error) {
 	conn, _, err := zk.Connect(servers, sessionTimeout)
 	if err != nil {
@@ -22,26 +22,50 @@ func New(servers []string, sessionTimeout time.Duration) (*Conn, error) {
 }
 
 // ChildrenC implements Zookeeper's getChildren method with watcher continuosly sending events.
-func (c *Conn) ChildrenC(path string) ([]string, *zk.Stat, <-chan zk.Event, error) {
-	return c.Conn.ChildrenW(path)
+func (c *Conn) ChildrenC(path string, refreshThreshold time.Duration) <-chan ChildrenEvent {
+	events := make(chan ChildrenEvent)
+	timer := time.NewTimer(refreshThreshold)
+
+	go func(path string) {
+		for {
+			children, stat, event, err := c.Conn.ChildrenW(path)
+			events <- ChildrenEvent{
+				Children: children,
+				Stat:     stat,
+				Err:      err,
+			}
+			select {
+			case e := <-event:
+				events <- ChildrenEvent{
+					Children: children,
+					Stat:     stat,
+					Evt:      &e,
+					Err:      err,
+				}
+			case <-timer.C:
+			}
+		}
+	}(path)
+
+	return events
 }
 
 // GetC implements Zookeeper's getData method with watcher continuosly sending events.
-func (c *Conn) GetC(path string, refreshThreshold time.Duration) <-chan Event {
-	events := make(chan Event)
+func (c *Conn) GetC(path string, refreshThreshold time.Duration) <-chan DataEvent {
+	events := make(chan DataEvent)
 	timer := time.NewTimer(refreshThreshold)
 
 	go func(path string) {
 		for {
 			data, stat, event, err := c.Conn.GetW(path)
-			events <- Event{
+			events <- DataEvent{
 				Data: data,
 				Stat: stat,
 				Err:  err,
 			}
 			select {
 			case e := <-event:
-				events <- Event{
+				events <- DataEvent{
 					Data: data,
 					Stat: stat,
 					Evt:  &e,
@@ -55,10 +79,18 @@ func (c *Conn) GetC(path string, refreshThreshold time.Duration) <-chan Event {
 	return events
 }
 
-// Event contains aggregate data from zk.ChildrenW.
-type Event struct {
+// DataEvent contains aggregated data from zk.GetW.
+type DataEvent struct {
 	Data []byte
 	Stat *zk.Stat
 	Evt  *zk.Event
 	Err  error
+}
+
+// ChildrenEvent contains aggregated data from zk.ChildrenW.
+type ChildrenEvent struct {
+	Children []string
+	Stat     *zk.Stat
+	Evt      *zk.Event
+	Err      error
 }
