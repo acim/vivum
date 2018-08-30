fbdc3e5e822b155fea0d79295bc4169c5c153da5
Boban Acimovic <boban.acimovic@gmail.com>
2018-08-30 20:42:42 +0200

    implement ExistsC

--- zookeeper.go
+++ zookeeper.go
@@ -9,22 +9,23 @@ import (
 // Conn extends go-zookeeper's functionality.
 type Conn struct {
 	*zk.Conn
+	rt time.Duration
 }
 
 // New creates new *zkc.Conn instance.
-func New(servers []string, sessionTimeout time.Duration) (*Conn, error) {
+func New(servers []string, sessionTimeout time.Duration, refreshThreshold time.Duration) (*Conn, error) {
 	conn, _, err := zk.Connect(servers, sessionTimeout)
 	if err != nil {
 		return nil, err
 	}
 
-	return &Conn{Conn: conn}, nil
+	return &Conn{Conn: conn, rt: refreshThreshold}, nil
 }
 
 // ChildrenC implements Zookeeper's getChildren method with watcher continuosly sending events.
-func (c *Conn) ChildrenC(path string, refreshThreshold time.Duration) <-chan ChildrenEvent {
+func (c *Conn) ChildrenC(path string) <-chan ChildrenEvent {
 	events := make(chan ChildrenEvent)
-	timer := time.NewTimer(refreshThreshold)
+	timer := time.NewTimer(c.rt)
 
 	go func(path string) {
 		for {
@@ -51,9 +52,9 @@ func (c *Conn) ChildrenC(path string, refreshThreshold time.Duration) <-chan Chi
 }
 
 // GetC implements Zookeeper's getData method with watcher continuosly sending events.
-func (c *Conn) GetC(path string, refreshThreshold time.Duration) <-chan DataEvent {
+func (c *Conn) GetC(path string) <-chan DataEvent {
 	events := make(chan DataEvent)
-	timer := time.NewTimer(refreshThreshold)
+	timer := time.NewTimer(c.rt)
 
 	go func(path string) {
 		for {
@@ -79,6 +80,35 @@ func (c *Conn) GetC(path string, refreshThreshold time.Duration) <-chan DataEven
 	return events
 }
 
+// ExistsC implements Zookeeper's exists method with watcher continuosly sending events.
+func (c *Conn) ExistsC(path string) <-chan ExistsEvent {
+	events := make(chan ExistsEvent)
+	timer := time.NewTimer(c.rt)
+
+	go func(path string) {
+		for {
+			exists, stat, event, err := c.Conn.ExistsW(path)
+			events <- ExistsEvent{
+				Exists: exists,
+				Stat:   stat,
+				Err:    err,
+			}
+			select {
+			case e := <-event:
+				events <- ExistsEvent{
+					Exists: exists,
+					Stat:   stat,
+					Evt:    &e,
+					Err:    err,
+				}
+			case <-timer.C:
+			}
+		}
+	}(path)
+
+	return events
+}
+
 // DataEvent contains aggregated data from zk.GetW.
 type DataEvent struct {
 	Data []byte
@@ -93,4 +123,12 @@ type ChildrenEvent struct {
 	Stat     *zk.Stat
 	Evt      *zk.Event
 	Err      error
+}
+
+// ExistsEvent contains aggregated data from zk.ExistsW.
+type ExistsEvent struct {
+	Exists bool
+	Stat   *zk.Stat
+	Evt    *zk.Event
+	Err    error
 }
